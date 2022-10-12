03b9186c1ee2604a6ab25aa241e57afb92827076
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-10-13 01:21:03 +0800

    added chart

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,33 @@
-import { quizContext } from "../Root/Root";
-
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
-  // const topics = useContext(quizContext);
+  const [quiz, setQuiz]= useState([])
+  useEffect(()=>{
+axios.get('https://openapi.programming-hero.com/api/quiz')
+.then(data=>{
+  const quizLoaded = data.data.data
+  const quizData = quizLoaded.map(quiz=>{
+    const singleQuiz ={
+      quiz: quiz.name,
+      total: quiz.total
+    }
+    return singleQuiz;
+  })
+  // console.log(quizData);
+  setQuiz(quizData)
+})
 
+  },[])
   return (
     <div>
-      <h1>this is chart</h1>
+      <BarChart width={500} height={500} data={quiz}>
+          <Bar dataKey="total" fill="#8884d8" />
+          <XAxis dataKey='quiz'fill="#8884d8"/>
+          <YAxis/>
+          <Tooltip></Tooltip>
+        </BarChart>
     </div>
   );
 };
