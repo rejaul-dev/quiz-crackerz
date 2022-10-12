a699a3835c31b2da091151dd0cec8405072fd6fe
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-10-12 22:28:00 +0800

    fixed some bugs

--- src/components/QuizDetails/QuizDetails.jsx
+++ src/components/QuizDetails/QuizDetails.jsx
@@ -1,37 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const QuizDetails = ({ quizDetail }) => {
   const { id, question, correctAnswer, options } = quizDetail;
 
-  const handleAnswer=()=>{
-    if(options===correctAnswer){
-      alert('correct')
+  const[open, setOpen]=useState(true)
+  const handleAnswer = (event) => {
+    if(event=== correctAnswer){
+      toast.success('correct')
     }else{
-      alert('wrong')
+      toast.warning('wrong')
     }
-  }
+  };
 
-  
   return (
     <section>
       <div className="md:w-3/5 w-5/6  mx-auto p-10 bg-secondary m-10 rounded-md ">
         <h1 className="text-xl font-bold">
           {question.replace(/(<([^>]+)>)/gi, "")}
         </h1>
-        {options.map((option) => (
+        {options.map((option, idx) =>  ( 
           <label className="bg-primary p-2 rounded-md mb-10 ">
             <input
-            onClick={()=>handleAnswer()}
+              onClick={()=>handleAnswer(option)}
               className="mt-7 mr-2 "
               type="radio"
-              value={correctAnswer}
+              value=''
               name={id}
             />
             {option}
             <br />
           </label>
         ))}
-      
       </div>
     </section>
   );
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,14 +3,11 @@ import { quizContext } from "../Root/Root";
 
 const Statistics = () => {
   const topics = useContext(quizContext);
-  const topicData = topics.map(topic=>topic)
-  
-   
-//  console.log(topics);
+
   return (
     <div className="bg-white p-20">
       <h1>This a chart</h1>
-     
+      
     </div>
   );
 };
